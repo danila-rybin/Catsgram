9cb3fa97ae3fd9a772c61bad0b83a0bb742c4b44
Danila Rybin <rybin.danila@gmail.com>
2025-10-13 01:01:18 +0300

    Добавление функциональности в Котограмм: завершили часть по работе с изображениями

--- src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
@@ -5,6 +5,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.yandex.practicum.catsgram.model.Image;
+import ru.yandex.practicum.catsgram.model.ImageData;
 import ru.yandex.practicum.catsgram.service.ImageService;
 
 import java.util.List;
@@ -17,6 +18,19 @@ public class ImageController {
     @GetMapping("/posts/{postId}/images")
     public List<Image> getPostImages(@PathVariable("postId") long postId) {
         return imageService.getPostImages(postId);
+    }
+
+    @GetMapping(value = "/images/{imageId}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> downloadImage(@PathVariable long imageId) {
+        ImageData imageData = imageService.getImageData(imageId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDisposition(
+                ContentDisposition.attachment()
+                        .filename(imageData.getName())
+                        .build()
+        );
+
+        return new ResponseEntity<>(imageData.getData(), headers, HttpStatus.OK);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
